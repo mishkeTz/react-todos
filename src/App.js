83f655b032ad2f65a165ac86545ac81c034f928c
Nikola Misic <mishke.deejay@gmail.com>
2019-02-19 21:00:55 +0100

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+
+import Todos from './containers/Todos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Container>
-          <Row>
-            <Col>1 of 2</Col>
-            <Col>2 of 2</Col>
-          </Row>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
-          </Row>
-        </Container>;
+        <Todos />
       </div>
     );
   }
